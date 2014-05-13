010477e0448af7a378c6e8a0ca82dd9f2b833bff
temporizer <temporizer@gmail.com>
2014-05-12 19:50:26 -0700

    First push

---
+++ includes/header.inc.php
@@ -0,0 +1,59 @@
+<?php 
+	include_once 'includes/functions.inc.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Skill Building 2</title>
+	<link rel="stylesheet" href="css/style.css" media="screen">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<link href='http://fonts.googleapis.com/css?family=IM+Fell+DW+Pica:400,400italic' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+</head>
+<body>
+	<section id="wrapper">
+		<header>
+			<div class="logo">
+				
+			</div> <!-- end logo -->
+			<div class="title">
+				
+			</div> <!-- end title -->
+			
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			<a class="navbar-brand" href="#">Beauty</a>
+			</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#christine">Christine</a></li>
+					<li><a href="#david">David</a></li>
+					<li><a href="#jino">Jino</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+
+		</header> <!-- end header -->
+
+		<section id="sidebar">
+
+		</section> <!-- end sidebar -->
+
+		<section id="content">
---
+++ index.php
@@ -0,0 +1,86 @@
+<?php include 'includes/header.inc.php'; ?>
+
+<section  class="text-center">
+	<h1>Christine</h1>
+</section>
+<div class="row">
+  <div class="col-sm-4">
+    <a href="#" class="thumbnail">
+      <img data-src="images/hawaii.jpg" src="images/hawaii.jpg" alt="...">
+    </a>
+  </div>
+  <div class="col-sm-4">
+    <a href="#" class="thumbnail">
+      <img data-src="images/manifest.jpg" src="images/manifest.jpg" alt="...">
+    </a>
+  </div>
+  <div class="col-sm-4">
+    <a href="#" class="thumbnail">
+      <img data-src="images/netarts_bay.jpg" src="images/netarts_bay.jpg" alt="...">
+    </a>
+  </div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">
+    Beauty is a perfect moment. The sun broke through the clouds like a glimmer of hope in an otherwise dark time. The scent of fresh rain mixed with the salty ocean breeze. Beauty is peace and joy and wonder. Beauty is simple.
+  </div>
+</div>
+
+
+<section class="page-header text-center">
+	<h1>David</h1>
+</section>
+
+<div class="row">
+  <div class="col-sm-4">
+    <a href="#" class="thumbnail">
+      <img data-src="images/hawaii.jpg" src="images/hawaii.jpg" alt="...">
+    </a>
+  </div>
+  <div class="col-sm-4">
+    <a href="#" class="thumbnail">
+      <img data-src="images/manifest.jpg" src="images/manifest.jpg" alt="...">
+    </a>
+  </div>
+  <div class="col-sm-4">
+    <a href="#" class="thumbnail">
+      <img data-src="images/netarts_bay.jpg" src="images/netarts_bay.jpg" alt="...">
+    </a>
+  </div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    Basic panel example
+  </div>
+</div>
+
+
+<section class="page-header text-center">
+	<h1>Jino</h1>
+</section>
+
+<div class="row">
+  <div class="col-sm-4">
+    <a href="#" class="thumbnail">
+      <img data-src="images/hawaii.jpg" src="images/hawaii.jpg" alt="...">
+    </a>
+  </div>
+  <div class="col-sm-4">
+    <a href="#" class="thumbnail">
+      <img data-src="images/manifest.jpg" src="images/manifest.jpg" alt="...">
+    </a>
+  </div>
+  <div class="col-sm-4">
+    <a href="#" class="thumbnail">
+      <img data-src="images/netarts_bay.jpg" src="images/netarts_bay.jpg" alt="...">
+    </a>
+  </div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    Basic panel example
+  </div>
+</div>
+<?php include 'includes/footer.inc.php'; ?>
