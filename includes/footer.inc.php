33a64a11816a327abee3c8422a0d27736439b595
temporizer <temporizer@gmail.com>
2014-05-14 19:03:29 -0700

    updated footer

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -7,19 +7,18 @@
 				<div class="col-md-4 text-right">
 					<h3>Christine</h3>
 					<ul>
-						<li><a href="http://www.facebook.com/temporizer"><i class="fa fa-facebook"></i>/temporizer</a></li>
-						<li><a href="http://www.twitter.com/jinoconklin"><i class="fa fa-twitter"></i>JinoConklin</a></li>
+						<li><a href="http://www.facebook.com/christine.watson2014"><i class="fa fa-facebook"></i>/christine.watson2014</a></li>
+						<li><a href="http://www.twitter.com/C2014Watson"><i class="fa fa-twitter"></i>C2014Watson</a></li>
 						<li><a href="mailto:temporizer@gmail.com">temporizer@gmail.com</a></li>
-						<li><a href="http://www.jinoconklin.com">jinoconklin.com</a></li>
+						<li><a href="http://44watdesign.wordpress.com">44watdesign.wordpress.com</a></li>
 					</ul>
 				</div>
 				<div class="col-md-4 text-center">
 					<h3>David</h3>
 					<ul>
-						<li><a href="http://www.facebook.com/temporizer"><i class="fa fa-facebook"></i>/temporizer</a></li>
-						<li><a href="http://www.twitter.com/jinoconklin"><i class="fa fa-twitter"></i>JinoConklin</a></li>
-						<li><a href="mailto:temporizer@gmail.com">temporizer@gmail.com</a></li>
-						<li><a href="http://www.jinoconklin.com">jinoconklin.com</a></li>
+						<li><a href="https://www.facebook.com/PhReEcAzOyD"><i class="fa fa-facebook"></i>/PhReEcAzOyD</a></li>
+						<li><a href="http://www.twitter.com/dell1437"><i class="fa fa-twitter"></i>dell1437</a></li>
+						<li><a href="mailto:d.larson3@students.clark.edu">d.larson3@students.clark.edu</a></li>
 					</ul>
 				</div>
 				<div class="col-md-4 text-left">
